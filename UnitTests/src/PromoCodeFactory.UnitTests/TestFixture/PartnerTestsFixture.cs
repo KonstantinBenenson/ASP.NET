5628f2f711a30b95156085e4bbb289af19eaa0e0
KonstantinBenenson <km.benenson@gmail.com>
2024-10-03 01:36:40 +0300

    initial commit

---
+++ UnitTests/src/PromoCodeFactory.UnitTests/TestFixture/PartnerTestsFixture.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.DataAccess;
+using PromoCodeFactory.DataAccess.Data;
+using PromoCodeFactory.DataAccess.Repositories;
+using PromoCodeFactory.WebHost.Controllers;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromoCodeFactory.UnitTests.TestFixture
+{
+    public class PartnerTestsFixture : IDisposable
+    {
+        public PartnersController PartnersController { get; }
+        public IRepository<Partner> PartnersRepository { get; private set; }
+        public IServiceProvider ServiceProvider { get; }
+        public DataContext DataContext { get; }
+        public PartnerTestsFixture()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<PartnersController>();
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddDbContext<DataContext>(x =>
+            {
+                x.UseSqlite("DataSource=:memory:;cache=shared");
+                x.UseSnakeCaseNamingConvention();
+                x.UseLazyLoadingProxies();
+            });
+
+            ServiceProvider = services.BuildServiceProvider();
+            DataContext = ServiceProvider.GetRequiredService<DataContext>();
+            // Open Connection and Ensure that all tables are created in DB
+            DataContext.Database.OpenConnection();
+            DataContext.Database.EnsureCreated();
+
+            Seed().GetAwaiter().GetResult();
+
+            PartnersController = ServiceProvider.GetRequiredService<PartnersController>();
+        }
+
+        private async Task Seed()
+        {
+            PartnersRepository = ServiceProvider.GetRequiredService<IRepository<Partner>>();
+            foreach (var partner in FakeDataFactory.Partners)
+            {
+                await PartnersRepository.AddAsync(partner);
+            }
+            await DataContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            DataContext.ChangeTracker.Clear();
+            DataContext.Database.CloseConnection();
+            DataContext.Dispose();
+        }
+    }
+}
--- UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,120 @@
-﻿namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.UnitTests.TestFixture;
+using PromoCodeFactory.WebHost.Controllers;
+using PromoCodeFactory.WebHost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
-    public class SetPartnerPromoCodeLimitAsyncTests
+    public class SetPartnerPromoCodeLimitAsyncTests : IClassFixture<PartnerTestsFixture>
     {
-        //TODO: Add Unit Tests
+        private PartnersController _partnerController;
+        private PartnerTestsFixture _partnerTestsFixture;
+        public SetPartnerPromoCodeLimitAsyncTests(PartnerTestsFixture partnerTestsFixture)
+        {
+            _partnerTestsFixture = partnerTestsFixture;
+        }
+
+        // Here I use a Moq as en example of how it can be used, when we need to make some related method return what we need. 
+        // Later I use real Repository and seeded Data.
+        [Theory]
+        [MemberData(nameof(TestData.GetPartnerNullAndInactiveData), MemberType = typeof(TestData))]
+        public async Task GetPartner_WhenIsNotFoundOrIsInactive_ReturnsError(Partner partner)
+        {
+            // Arrange 
+            var partnersRepoMoq = new Mock<IRepository<Partner>>();
+            partnersRepoMoq.Setup(c => c.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(partner);
+            _partnerController = new(partnersRepoMoq.Object);
+            var newLimit = 40;
+            var request = new SetPartnerPromoCodeLimitRequest { Limit = newLimit, EndDate = DateTime.UtcNow.AddDays(3) };
+
+            // Act
+            var result = await _partnerController.SetPartnerPromoCodeLimitAsync(Guid.NewGuid(), request);
+
+            // Assert
+            result.Should().NotBeNull();
+            if (partner is null)
+                result.Should().BeOfType<NotFoundResult>();
+            else
+                result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.GetPartnerWithActiveLimit), MemberType = typeof(TestData))]
+        public async Task SetLimit_AddLimitToActivePartner_LimitIsAdded(Partner partner)
+        {
+            // Arrange 
+            var partnerId = partner.Id;
+            var partnersController = _partnerTestsFixture.PartnersController;
+            var newLimit = 40;
+            var request = new SetPartnerPromoCodeLimitRequest { Limit = newLimit, EndDate = DateTime.UtcNow.AddDays(3) };
+
+            // Act
+            var result = await partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+            var partnerAfterUpdate = await _partnerTestsFixture.PartnersRepository.GetByIdAsync(partnerId);
+            var partnerLimit = partnerAfterUpdate.PartnerLimits.LastOrDefault();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<CreatedAtActionResult>();
+            partnerLimit.CreateDate.Date.Should().Be(DateTime.Now.Date);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.GetPartnerWithActiveLimit), MemberType = typeof(TestData))]
+        public async Task SetLimit_PartnerWithActiveLimits_PromoCodesCountBecomes_0(Partner partner)
+        {
+            // Arrange 
+            var partnerId = partner.Id;
+            var partnersController = _partnerTestsFixture.PartnersController;
+            var newLimit = 40;
+            var request = new SetPartnerPromoCodeLimitRequest { Limit = newLimit, EndDate = DateTime.UtcNow.AddDays(3) };
+
+            // Act
+            var result = await partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+            var partnerAfterUpdate = await _partnerTestsFixture.PartnersRepository.GetByIdAsync(partnerId);
+            var promoCodes = partnerAfterUpdate.NumberIssuedPromoCodes;
+            // Take the second from the end limit, which should had been active before we added a new one
+            var prevLimit = (partnerAfterUpdate.PartnerLimits as List<PartnerPromoCodeLimit>)[partnerAfterUpdate.PartnerLimits.Count-2];
+            var limit = partnerAfterUpdate.PartnerLimits.LastOrDefault()?.Limit;
+
+            // Assert
+            result.Should().NotBeNull();
+            prevLimit.CancelDate.Should().NotBeNull(); // checking the previous Limit CancelDate was set
+            limit.Should().Be(newLimit); // Limit now is renewed
+            promoCodes.Should().Be(0);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.GetPartnerWithInactiveLimit), MemberType = typeof(TestData))]
+        public async Task SetLimit_NoActiveLimits_PromoCodesCountIsNotChanged(Partner partner)
+        {
+            // Arrange 
+            var partnerId = partner.Id;
+            var partnersController = _partnerTestsFixture.PartnersController;
+            var initialNumberOfPromoCodes = partner.NumberIssuedPromoCodes;
+            var newLimit = 40;
+            var request = new SetPartnerPromoCodeLimitRequest { Limit = newLimit, EndDate = DateTime.UtcNow.AddDays(3) };
+            var partnerBeforeUpdate = await _partnerTestsFixture.PartnersRepository.GetByIdAsync(partnerId);
+
+            // Act
+            var result = await partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+            var partnerAfterUpdate = await _partnerTestsFixture.PartnersRepository.GetByIdAsync(partnerId);
+            var promoCodesAfterUpdate = partnerAfterUpdate.NumberIssuedPromoCodes;
+
+            // Assert
+            result.Should().NotBeNull();
+            promoCodesAfterUpdate.Should().Be(initialNumberOfPromoCodes);
+        }
     }
+
+    //test
 }
